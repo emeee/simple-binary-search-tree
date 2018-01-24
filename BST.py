8e194c5f5402abd1729cbdfe9b0cfeebda28ddf9
Emiliano Gallardo <emiliano.gallardo@phinxlab.com>
2018-01-24 18:57:47 -0300

    Modularize

--- BST.py
+++ BST.py
@@ -1,41 +1,4 @@
-class Node:
-
-    def __init__(self, key, val, leftChild=None, rightChild=None, parent=None):
-        self.key = key
-        self.val = val
-        self.leftChild = leftChild
-        self.rightChild = rightChild
-        self.parent = parent
-
-    def getVal(self):
-        return self.val
-
-    def getKey(self):
-        return self.key
-
-    def getLeftChild(self):
-        return self.leftChild
-
-    def getRightChild(self):
-        return self.rightChild
-
-    def getParent(self):
-        return self.parent
-
-    def setVal(self, val):
-        self.val = val
-
-    def setLeftChild(self, leftChild):
-        self.leftChild = leftChild
-
-    def setRightChild(self, rightChild):
-        self.rightChild = rightChild
-
-    def setParent(self, parent):
-        self.parent = parent
-
-    def isLeaf(self):
-        return not(self.rightChild or self.leftChild)
+from node import Node
 
 class BST:
 
@@ -51,7 +14,7 @@ class BST:
             self._insert(key, val, self.root)
         else:
             self.root = Node(key ,val)
-        self.size = self.size + 1
+        self.size += 1
 
     def _insert(self, key, val, currentNode):
         if key < currentNode.getKey():
@@ -86,7 +49,7 @@ class BST:
             nodeToRemove = self._get(key, self.root)
             if nodeToRemove:
                 self._delete(nodeToRemove)
-                self.size = self.size - 1
+                self.size -= 1
             else:
                 raise('Key not found')
         elif self.size == 1 and self.root.key == key:
---
+++ node.py
@@ -0,0 +1,38 @@
+class Node:
+
+    def __init__(self, key, val, leftChild=None, rightChild=None, parent=None):
+        self.key = key
+        self.val = val
+        self.leftChild = leftChild
+        self.rightChild = rightChild
+        self.parent = parent
+
+    def getVal(self):
+        return self.val
+
+    def getKey(self):
+        return self.key
+
+    def getLeftChild(self):
+        return self.leftChild
+
+    def getRightChild(self):
+        return self.rightChild
+
+    def getParent(self):
+        return self.parent
+
+    def setVal(self, val):
+        self.val = val
+
+    def setLeftChild(self, leftChild):
+        self.leftChild = leftChild
+
+    def setRightChild(self, rightChild):
+        self.rightChild = rightChild
+
+    def setParent(self, parent):
+        self.parent = parent
+
+    def isLeaf(self):
+        return not(self.rightChild or self.leftChild)
